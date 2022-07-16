f7cb9f0ad1963c945551717f941130e13077ec6e
imperatrona <xb2w1z@gmail.com>
2022-07-16 03:38:29 +0300

    Initial Commit

---
+++ Video Probe/Video_ProbeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Video_ProbeApp.swift
+//  Video Probe
+//
+//  Created by Алексей Ботвин on 7/16/22.
+//
+
+import SwiftUI
+
+@main
+struct Video_ProbeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
